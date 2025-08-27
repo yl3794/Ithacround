90c9ca96f93805e377013334c1cb66f180c767e6
Ye <yl3794@cornell.edu>
2025-08-27 00:08:13 -0400

    Initial Commit

---
+++ Ithacaround-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ithacaround-Demo
+//
+//  Created by Kiera Lin on 8/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ithacaround-Demo/Ithacaround_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ithacaround_DemoApp.swift
+//  Ithacaround-Demo
+//
+//  Created by Kiera Lin on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Ithacaround_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
